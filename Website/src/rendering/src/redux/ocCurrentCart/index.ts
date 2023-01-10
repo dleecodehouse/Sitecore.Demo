9ecb6a2377723b1b47e3e3ed504da8126378f5eb
Dennis Lee <dle@codehousegroup.com>
2023-01-10 20:22:42 +1100

    adding logging

--- Website/src/rendering/src/redux/ocCurrentCart/index.ts
+++ Website/src/rendering/src/redux/ocCurrentCart/index.ts
@@ -29,6 +29,7 @@ import {
   COOKIES_ANON_ORDER_PROMOS,
   COOKIES_ANON_USER_TOKEN,
 } from '../../constants/cookies';
+import { Token } from 'graphql';
 
 export interface RecentOrder {
   order: RequiredDeep<DOrder>;
@@ -85,11 +86,35 @@ export const updateCreditCardPayment = createOcAsyncThunk<
       SpendingAccount: null,
     },
   };
+
+  const token = Tokens.GetAccessToken();
+  console.log('token: ' + token);
+  console.log(payment);
+
   const response = await axios.put<RequiredDeep<DPayment>[]>(
     `/api/checkout/update-payments/${order.ID}`,
     { Payments: [payment] },
-    { headers: { Authorization: `Bearer ${Tokens.GetAccessToken()}` } }
-  );
+    { headers: { Authorization: `Bearer ${token}` } }
+  ).catch(function(err) {
+    if (err.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else if (err.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(err.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', err.message);
+    }
+    console.log(err.config);
+  });
+  console.log('axios data');
+  console.log(response);
   return response.data;
 });
 
